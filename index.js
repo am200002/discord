d885ea97278a1b14010ef2bf981512e88e4ef6b9
am200002 <76538988+am200002@users.noreply.github.com>
2020-12-24 00:27:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -133,4 +133,4 @@ function play(guild, song) {
   dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
   serverQueue.textChannel.send(`Start playing: **${song.title}**`);
 }
-client.login('process.env.TOKEN')
+client.login('process.env.KEY')
